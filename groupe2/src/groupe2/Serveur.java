55b624c170362e33c4c80d30a1cddbac3f3b7bdb
theamartinez <theamartinez@192.168.173.208>
2019-03-01 18:02:16 +0100

    file corrected

--- groupe2/src/groupe2/Serveur.java
+++ groupe2/src/groupe2/Serveur.java
@@ -4,54 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.io.PrintWriter;
 
-public class Serveur{
-    
-    public static void main(String[] zero) {
-        
-        ServerSocket socketserver = null  ;
-        Socket socketduserveur ;
-        BufferedReader in;
-        PrintWriter out;
-        
-        try {
-        
-            socketserver = new ServerSocket(socketserver.getLocalPort());
-            socketduserveur = socketserver.accept(); 
-                System.out.println("connexion");
-            in = new BufferedReader (new InputStreamReader (socketduserveur.getInputStream()));
-            out = new PrintWriter(socketduserveur.getOutputStream());
-            String message_distant = in.readLine();
-            System.out.println(message_distant);
-                out.println(message);
-                out.flush();
-               
-               
-                        
-                socketduserveur.close();
-                socketserver.close();
-                
-        }catch (IOException e) {
-            
-            e.printStackTrace();
-        }
-    }
-
-}
-/*import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.io.PrintWriter;
 
 public class Serveur {
 	
@@ -64,12 +19,12 @@ public class Serveur {
 		
 		try {
 		
-			socketserver = new ServerSocket(2009);
-			System.out.println("Le serveur est à l'écoute du port "+socketserver.getLocalPort());
+			socketserver = new ServerSocket(2019);
+			System.out.println("Connectez-vous!");
 			socketduserveur = socketserver.accept(); 
-		        System.out.println("Un zéro s'est connecté");
+		        System.out.println("Un joueur s'est connecté");
 			out = new PrintWriter(socketduserveur.getOutputStream());
-		        out.println("Vous êtes connecté zéro !");
+		        out.println("Vous êtes connecté !");
 		        out.flush();
 		                
 		        socketduserveur.close();
@@ -83,4 +38,4 @@ public class Serveur {
 
 }
 
-*/
+
