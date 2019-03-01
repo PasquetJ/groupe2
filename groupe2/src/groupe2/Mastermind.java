0a080697946fcdd49679bce09cd31f65430bb1a4
theamartinez <theamartinez@192.168.173.208>
2019-03-01 17:41:36 +0100

    random colors

--- groupe2/src/groupe2/Mastermind.java
+++ groupe2/src/groupe2/Mastermind.java
@@ -1,4 +1,4 @@
-package groupe2;
+package Partie1;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,44 +6,39 @@ import java.util.Random;
 
 
 public class Mastermind {
-	private String a;
-	private String b;
-	private String c;
-	private String d;
-	private List <String> combi = new ArrayList<String>();
-	private List <String> hasard = new ArrayList<String>();
+
+	private List <Integer> hasard = new ArrayList<Integer>();
 	
 	
 		
-	public Mastermind(String a, String b, String c, String d) {
-		
-		this.a = a;
-		this.b = b;
-		this.c = c;
-		this.d = d;
-		
+	public Mastermind() {
+
+		createList();
 	}
 	
-	public Mastermind(List<String> combi){
-		for( int i=0;i<combi.size();i++){
-			//int nbrRandom = Math.random() * ( (comb.size())-1 - 0);
-			//combi.add(couleurpossible[nbrRandom];
-			combi.add(a);
-			combi.add(b);
-			combi.add(c);
-			combi.add(d);
-			
-			int indiceAuHasard = (int )(Math.random()*combi.size()-1);
-			//hasard.add(indiceAuHasard);
-			String value = combi.get(1);
-			System.out.println(value);
-		}}	
+	public void createList(){
+		for( int i=0;i<4;i++){
 
+			
+			int indiceAuHasard = (int) (Math.random()*(Colors.values().length+1)-1);
+			hasard.add(new Integer(indiceAuHasard));
+			//String value = combi.get(1);
+			
+		}
+	}
+
+	public String toString() {
+		String r = "";
+		for (Integer i : hasard) {
+			r += Colors.values()[i] + ", ";
+		}
+		return r;
+	}
 	
 	public static void main(String [] args){
 		//String [] combi= "bleu rouge noir jaune".split(" ");
 		//System.out.println(combi);
-		Mastermind m1= new Mastermind("bleu", "rouge","noir","jaune");
+		Mastermind m1= new Mastermind();
 		System.out.println(m1);
 		//return combi.index[0];
 		//System.out.println("Position 1: "+ combi.get(1));
