6476d79b39f5592fb2a56de250c569352d355157
theamartinez <theamartinez@192.168.173.208>
2019-03-01 18:13:29 +0100

    correction

--- groupe2/src/groupe2/Client.java
+++ groupe2/src/groupe2/Client.java
@@ -3,75 +3,6 @@ package groupe2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
-
-
-public class Client {
-    
-    public static void main(String[] zero) {
-        
-        int a;
-        Socket socket;
-        BufferedReader in;
-        PrintWriter out;
-
-        try {
-        
-            socket = new Socket(InetAddress.getLocalHost(),2018);   
-                System.out.println("Demande de connexion");
-                out = new PrintWriter(socket.getOutputStream());
-                in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
-                
-                String message_distant = in.readLine();
-                out.println("Bonjour");
-                System.out.println(message_distant);
-                
-                String message_distant1 = in.readLine();
-                System.out.println(message_distant1);
-                
-                Scanner sc = new Scanner(System.in);
-                String message = sc.next();
-                out = new PrintWriter(socket.getOutputStream());
-                out.println(message);
-                out.flush();
-                
-                
-                socket.close();
-          
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-        }catch (UnknownHostException e) {
-            
-            e.printStackTrace();
-        }catch (IOException e) {
-            
-            e.printStackTrace();
-        }
-    }
-
-}
-/* import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -84,11 +15,11 @@ public class Client {
 		
 		Socket socket;
 		BufferedReader in;
-		PrintWriter out;
+		
 
 		try {
 		
-			socket = new Socket(InetAddress.getLocalHost(),2009);	
+			socket = new Socket(InetAddress.getLocalHost(),2019);	
 		        System.out.println("Demande de connexion");
 
 		        in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
@@ -107,5 +38,3 @@ public class Client {
 	}
 
 }
-
-*/
